5748f1805ceacf9dfef23f692667b14306ea62f8
Gredding <51574071+Gredding@users.noreply.github.com>
2021-10-31 21:07:43 +0200

    Update config
    deleted process.env

--- gatsby-config.js
+++ gatsby-config.js
@@ -31,8 +31,8 @@ module.exports = {
     {
       resolve: `gatsby-source-contentful`,
       options: {
-        spaceId: process.env.CONTENTFUL_SPACE_ID,
-        accessToken: process.env.CONTENTFUL_ACCESS_TOKEN,
+        spaceId: CONTENTFUL_SPACE_ID,
+        accessToken: CONTENTFUL_ACCESS_TOKEN,
         downloadLocal: true,
       },
     },
